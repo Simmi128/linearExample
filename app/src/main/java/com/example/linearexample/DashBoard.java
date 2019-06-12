830aef75d7b6cf5910531a564d92baf9c9dd451a
1894166 <Simmi128>
2019-06-12 21:53:47 +0530

    demo

--- app/src/main/java/com/example/linearexample/DashBoard.java
+++ app/src/main/java/com/example/linearexample/DashBoard.java
@@ -1,13 +1,34 @@
 package com.example.linearexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DashBoard extends AppCompatActivity {
 
+
+    String unm ,upass;
+    TextView txt_nm,txt_pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        Bundle bundle = getIntent().getExtras();
+        unm = bundle.getString("un");
+        upass= bundle.getString("ps");
+
+
+        Intent i = getIntent();
+
+        unm= i.getStringExtra("un");
+        upass= i.getStringExtra("ps");
+
+        txt_nm = findViewById(R.id.txt_uname);
+        txt_pass = findViewById(R.id.txt_pass);
+
+        txt_nm.setText(unm);
+        txt_pass.setText(upass);
     }
 }
--- app/src/main/java/com/example/linearexample/MainActivity.java
+++ app/src/main/java/com/example/linearexample/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.linearexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText edt_uname,edt_pass;
+    Button btn_submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        edt_uname= findViewById(R.id.text);
+        edt_pass= findViewById(R.id.pass);
+
+        btn_submit= findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                String uname = edt_uname.getText().toString();
+                String pass = edt_pass.getText().toString();
+
+                Bundle bundle = new Bundle();
+
+                bundle.putString("un",uname);
+                bundle.putString("ps",pass);
+
+
+                Intent i = new Intent(MainActivity.this,DashBoard.class);
+                 i.putExtras(bundle);
+                startActivity(i);
+
+                /*new Intent(MainActivity.this,DashBoard.class);
+
+                i.putExtra("un",uname);
+                i.putExtra("ps",pass);
+
+
+                startActivity(i);*/
+            }
+             });
+
     }
 }
